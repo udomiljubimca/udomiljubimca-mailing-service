16f04ac0f1304a192a81afd7c47ac3199f97cd87
Mifa43 <mifa43kotez@gmail.com>
2021-08-05 09:23:16 +0200

    sredjivanje konflikta

--- src/send_email.py
+++ src/send_email.py
@@ -30,26 +30,3 @@ class EmailToSend():
         text = msg.as_string()
         server.sendmail(from_addr, to, text)
         return {"email_sended": True}
-    def send_multiple(self):
-        sender_email = os.getenv('EMAIL')
-        sender_password = os.getenv('PASSWORD')
-        smtp_host = os.getenv('SMTP_HOST')
-        smtp_port = os.getenv('SMTP_PORT')
-        from_addr = sender_email
-        to = self.reciver_email  
-
-        msg = MIMEMultipart()
-        msg["from_addr"] = from_addr
-        msg["to"] = to 
-        msg["Subject"] = self.title
-
-        body = self.message
-        msg.attach(MIMEText(body, "Plain"))
-
-        server = smtplib.SMTP(smtp_host, smtp_port)
-        server.ehlo()
-        server.starttls()
-        server.login(sender_email, sender_password)
-        text = msg.as_string()
-        server.sendmail(from_addr, to, text)
-        return {"email_sended": True}
